4d091051dadf27a0f6b74987db6ee837df2f99ac
max <qdqwqwdqd@gmail.com>
2026-01-30 15:29:48 +0200

    змінили задній фон

--- client.py
+++ client.py
@@ -41,7 +41,8 @@ def receive():
 font_win = font.Font(None, 72)
 font_main = font.Font(None, 36)
 # --- ЗОБРАЖЕННЯ ----
-
+background = image.load("images/bg.jpg")
+background = transform.scale(background,(WIDTH,HEIGHT))
 # --- ЗВУКИ ---
 
 # --- ГРА ---
@@ -88,7 +89,7 @@ while True:
         continue  # Блокує гру після перемоги
 
     if game_state:
-        screen.fill((30, 30, 30))
+        screen.blit(background,(0,0))
         draw.rect(screen, (0, 255, 0), (20, game_state['paddles']['0'], 20, 100))
         draw.rect(screen, (255, 0, 255), (WIDTH - 40, game_state['paddles']['1'], 20, 100))
         draw.circle(screen, (255, 255, 255), (game_state['ball']['x'], game_state['ball']['y']), 10)
